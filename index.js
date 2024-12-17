58f4d752f2dd44b67c653ae8e784490a5f036c1c
katherinhernandez <andreakatherinhernandez@alumnat.iesdamiahuguet.net>
2024-12-17 12:25:46 +0100

    commit inicial

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+function concatena(){
+    let Nom = document.getElementById("CampNom").value;
+    let Llinatges = document.getElementById("CampLlinatges").value;
+    alert(Nom + " " + Llinatges);
+}
